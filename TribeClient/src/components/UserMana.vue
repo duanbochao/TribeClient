95e425e7201940d2c5df5bb0e800b640101a37e4
duan <1842988062@qq.com>
2020-01-18 09:27:20 +0800

    完成了用户管理中的查询和角色显示功能

--- TribeClient/src/components/UserMana.vue
+++ TribeClient/src/components/UserMana.vue
@@ -1,17 +1,155 @@
 <template>
+  <div>
     <div>
-
+      <el-input
+        v-model="nickName"
+        placeholder="请输入内容"
+        style="width:35%"
+        size="small"
+      ></el-input>
+      <el-button type="primary" icon="el-icon-search" size="small"   @click="getDate">搜索</el-button>
     </div>
+
+    <div class="box">
+      <el-card class="box-card" v-for="(item,index) in tableData" :key="index">
+        <div slot="header" class="clearfix">
+          <span>{{item.nickname}}</span>
+          <el-button
+            style="float: right; padding: 3px 0;color:#ff0509"
+            type="text"
+            class="el-icon-delete"
+          >&nbsp;删除</el-button>
+        </div>
+        <div class="item text">
+          <div>
+            <span>用户名:</span>
+            <span>{{item.username}}</span>
+          </div>
+
+          <div>
+            <span>电子邮箱:</span>
+            <span>{{item.email}}</span>
+          </div>
+
+          <div>
+            <span>注册时间:</span>
+            <span>{{item.regTime }}</span>
+          </div>
+
+          <div>
+            <span>用户状态:</span>
+            <el-switch v-model="item.enabled" active-text="启用" inactive-text="禁用"></el-switch>
+          </div>
+          <div>
+            <span>用户角色:</span>
+            <el-tag
+              type="success"
+              size="mini"
+              v-for="(tags,index) in item.roles"
+              :key="index"
+            >{{tags.name}}</el-tag>
+
+            <el-popover placement="right" width="200" trigger="click" title="角色列表">
+              <el-select v-model="selectRoles" multiple placeholder="请选择">
+                <el-option
+                  v-for="item in allRoles"
+                  :key="item.id"
+                  :label="item.name"
+                  :value="item.id"
+                ></el-option>
+              </el-select>
+              <el-button
+                icon="el-icon-more"
+                style="padding-top: 0px"
+                slot="reference"
+                size="mini"
+                @click="showRole(item.roles)"
+              ></el-button>
+            </el-popover>
+          </div>
+        </div>
+      </el-card>
+    </div>
+  </div>
 </template>
 <script>
 export default {
-   data() {
-      return {
-      }
-   },
-   activated() {
-   }
-}
+  data() {
+    return {
+      allRoles: [],
+      selectRoles: [],
+      disabled: false,
+      tableData: [],
+      input: "",
+      nickName: ""
+    };
+  },
+  created() {
+    this.getDate();
+    this.getAllRoles();
+  },
+  methods: {
+    hello(){
+      console.log("helllo world");
+      
+    },
+    getDate() {
+      console.log("========");
+      var _this = this;
+      this.postRequest("/admin/user",{
+        nickName:_this.nickName
+      }).then(resp => {
+        if (resp.status == 200) {
+          _this.tableData = resp.data;
+          // _this.enabled=res.data.enabled;
+        }
+      });
+    },
+    showRole(roles) {
+      var selectRole = [];
+      roles.forEach(r => {
+        selectRole.push(r.name);
+      });
+
+      this.selectRoles = selectRole;
+    },
+    getAllRoles() {
+      var _this = this;
+      this.getRequest("/admin/roles").then(resp => {
+        if (resp.status == 200) {
+          _this.allRoles = resp.data;
+          console.log(_this.allRoles);
+        }
+      });
+    }
+  }
+};
 </script>
 <style scoped>
+.box-card {
+  width: 350px;
+  height: 300px;
+  margin-bottom: 10px;
+  text-align: left;
+}
+.item {
+  margin-top: 105px;
+}
+
+.box {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  background-color: yellow;
+  justify-content: space-around;
+  align-content: center;
+  flex-wrap: wrap;
+  /* margin-top:10px; */
+  padding: 20px 10px;
+}
+
+.text {
+  font-size: 12px;
+  color: blue;
+}
 </style>
